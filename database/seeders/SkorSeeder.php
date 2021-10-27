f150d5bbf2b19f70bf93f1497e2b8a3bca926ed2
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-10-27 19:03:05 +0800

    Menambahkan Seeder

---
+++ database/seeders/SkorSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Skors;
+use Illuminate\Database\Seeder;
+
+class SkorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Skors::create(['skor' => 30]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 30]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 55]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 30]);
+        Skors::create(['skor' => 50]);
+        Skors::create(['skor' => 40]);
+        Skors::create(['skor' => 50]);
+        Skors::create(['skor' => 50]);
+        Skors::create(['skor' => 50]);
+        Skors::create(['skor' => 55]);
+        Skors::create(['skor' => 50]);
+        Skors::create(['skor' => 45]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 45]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 65]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 80]);
+        Skors::create(['skor' => 60]);
+        Skors::create(['skor' => 80]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 70]);
+        Skors::create(['skor' => 90]);
+        Skors::create(['skor' => 80]);
+        Skors::create(['skor' => 90]);
+        Skors::create(['skor' => 80]);
+        Skors::create(['skor' => 90]);
+        Skors::create(['skor' => 80]);
+        Skors::create(['skor' => 90]);
+    }
+}
