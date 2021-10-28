29ba7e3a754772ad1ae5ea8fd603f38afa036ae3
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-10-28 13:34:54 +0800

    asdasd

--- app/View/Components/appLayout.php
+++ app/View/Components/appLayout.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class app extends Component
+class appLayout extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class app extends Component
      */
     public function render()
     {
-        return view('layouts.App');
+        return view('layouts/app-layout');
     }
 }
